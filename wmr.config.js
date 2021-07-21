458b82a0e361146b6036a2210deab969e2849d94
Arne Wiese <arne@wiese.me>
2021-07-21 19:50:22 +0200

    Inline critical css with critters

---
+++ wmr.config.js
@@ -0,0 +1,36 @@
+import Critters from "critters";
+import { defineConfig } from "wmr";
+
+function inlineCss() {
+  let cwd = ".";
+  return {
+    name: "inline-css-plugin",
+    enforce: "post",
+    configResolved(config) {
+      cwd = config.cwd;
+    },
+    async generateBundle(_, bundle) {
+      const critters = new Critters({
+        path: "",
+      });
+
+      // @ts-ignore
+      critters.readFile = async (f) => {
+        const localPath = f.replace(`${cwd}/`, "");
+        return bundle[localPath].source;
+      };
+
+      for (const id in bundle) {
+        const asset = bundle[id];
+        if (asset.type !== "asset" || !/\.html$/.test(asset.fileName)) continue;
+        asset.source = await critters.process(asset.source);
+      }
+    },
+  };
+}
+
+// @ts-ignore
+export default defineConfig({
+  // @ts-ignore
+  plugins: [inlineCss()],
+});
