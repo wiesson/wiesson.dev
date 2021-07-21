ba66938ac737ea5cb9ccc2d85fb6cf1d56721ed2
Arne Wiese <arne@wiese.me>
2021-07-21 22:38:06 +0200

    Cleanup wmr.config.js

--- wmr.config.js
+++ wmr.config.js
@@ -1,31 +1,7 @@
-import Critters from "critters";
-import { defineConfig } from "wmr";
-
-function inlineCss() {
-  let cwd = ".";
+export default function () {
   return {
-    name: "inline-css-plugin",
-    enforce: "post",
-    configResolved(config) {
-      cwd = config.cwd;
-    },
-    async generateBundle(_, bundle) {
-      const critters = new Critters({});
-
-      // @ts-ignore
-      critters.readFile = async (f) => {
-        const localPath = f.replace(`${cwd}/`, "");
-        return bundle[localPath].source;
-      };
-
-      for (const id in bundle) {
-        const asset = bundle[id];
-        if (asset.type !== "asset" || !/\.html$/.test(asset.fileName)) continue;
-        asset.source = await critters.process(asset.source);
-      }
+    alias: {
+      "src/*": "src",
     },
   };
 }
-
-// @ts-ignore
-export default defineConfig({});
