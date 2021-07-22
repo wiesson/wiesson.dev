1bb29332f24f41a9ad14ead03e3687fe20f45309
Arne Wiese <arne@wiese.me>
2021-07-22 09:58:38 +0200

    Update postProcessing script

--- postProcess.js
+++ postProcess.js
@@ -1,11 +1,9 @@
 import { readFileSync, writeFileSync } from "fs";
 import Critters from "critters";
 
-const htmlFilePath = "dist/index.html";
+const indexHtmlFileLocation = "dist/index.html";
 
 const critters = new Critters({ path: "dist/" });
-
-const f = readFileSync(htmlFilePath);
-
-const result = await critters.process(f.toString());
-writeFileSync(htmlFilePath, result);
+const indexHtmlContent = readFileSync(indexHtmlFileLocation).toString();
+const inlinedCssHtmlContent = await critters.process(indexHtmlContent);
+writeFileSync(indexHtmlFileLocation, inlinedCssHtmlContent);
