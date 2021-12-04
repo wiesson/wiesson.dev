079ef5fe7dc97b3e35fb63a94fd43dbf0d54ea46
Arne Wiese <arne@wiese.me>
2021-12-04 23:17:27 +0100

    Switch html lang to de

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
 import {
-  Link,
   Links,
   LiveReload,
   Meta,
@@ -106,7 +105,7 @@ function Document({
   const includeScripts = matches.some((match) => match.handle?.hydrate);
 
   return (
-    <html lang="en">
+    <html lang="de">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
