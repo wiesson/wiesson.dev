a2793330e50d535b0963697d2a0474d341835381
Arne Wiese <arne@wiese.me>
2022-01-26 14:12:03 +0100

    Fix rewrites syntax

--- next.config.js
+++ next.config.js
@@ -6,14 +6,16 @@ module.exports = {
     formats: ["image/avif", "image/webp"],
   },
   swcMinify: true,
-  rewrites: [
-    {
-      source: "/js/script.js",
-      destination: "https://plausible.io/js/plausible.js",
-    },
-    {
-      source: "/api/event",
-      destination: "https://plausible.io/api/event",
-    },
-  ],
+  async rewrites() {
+    return [
+      {
+        source: "/js/script.js",
+        destination: "https://plausible.io/js/plausible.js",
+      },
+      {
+        source: "/api/event",
+        destination: "https://plausible.io/api/event",
+      },
+    ];
+  },
 };
