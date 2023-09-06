4b69228dc782a95b4fff754c8d36926451f39e7a
Arne Wiese <arne@wiese.me>
2023-09-06 20:33:16 +0200

    Update to Astro 3

--- astro.config.js
+++ astro.config.js
@@ -1,8 +1,7 @@
 import preact from "@astrojs/preact";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from '@astrojs/image';
 
 export default defineConfig({
-  integrations: [preact(), tailwind(), image()],
+  integrations: [preact(), tailwind()],
 });
